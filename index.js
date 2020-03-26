4c856740bc3325c056d317a3150ac27d60ef9cd7
Wilson Ng <wilsongch92@gmail.com>
2020-03-26 13:21:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for(var i = 0; i < musicians.length; i += 1) {
-    array.push(musicians[i] + ' plays ' + instruments[i])
+    array.push(musicians[i] + ' plays ' + instruments[i]) //concatenation and push method
   }
-  return array
+  return array //return array after the loops are done performing.
+}
+
+function johnLennonFacts(arg) {
+  
 }
